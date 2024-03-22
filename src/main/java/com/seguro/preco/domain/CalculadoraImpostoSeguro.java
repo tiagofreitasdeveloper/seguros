6d352474b885c83ca38b8a577bd03dfd6cf8339f
tiago freitas <tiagofreitas.developer@hotmail.com>
2024-03-22 13:46:24 -0300

    ajuste no response do recurso que atualiza seguro

--- src/main/java/com/seguro/preco/domain/CalculadoraImpostoSeguro.java
+++ src/main/java/com/seguro/preco/domain/CalculadoraImpostoSeguro.java
@@ -5,6 +5,8 @@ import java.math.BigDecimal;
 public class CalculadoraImpostoSeguro {
 
     public static BigDecimal calcular(BigDecimal preco, BigDecimal iof, BigDecimal pis, BigDecimal cofins) {
-        return preco.multiply(BigDecimal.ONE.add(iof).add(pis).add(cofins)).setScale(2, BigDecimal.ROUND_UP);
+        return preco.multiply(BigDecimal.ONE.add(iof).add(pis).add(cofins))
+                .setScale(2, BigDecimal.ROUND_UP); // incluído ROUND_UP deprecated,
+        // porque o RoundMode por algum motivo nâo está acessĩvel no java.math dessa versão do java
     }
 }
